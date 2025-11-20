04fa4ec8e5d2473b5c4bd63e9ef171af3f78c5ca
Linda <jeropkoimur@gmail.com>
2025-11-20 13:28:07 +0300

    email parsing

--- lib/email_address_parser.py
+++ lib/email_address_parser.py
@@ -1,1 +1,15 @@
-# your code goes here!
+import re
+
+class EmailAddressParser:
+    def __init__(self, email_string):
+        self.email_string = email_string  # Store the string of emails
+    
+    def parse(self):
+        # Split by commas and/or spaces using regex
+        parts = re.split(r'[,\s]+', self.email_string)
+        
+        # Filter to keep only valid email addresses (must contain @)
+        emails = [part for part in parts if '@' in part]
+        
+        # Return unique emails, sorted alphabetically (set removes duplicates)
+        return sorted(set(emails))
